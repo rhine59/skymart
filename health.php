9313c30e80ae8e119b2bd47db6019ea700dd97e4
Richard Hine <rhine59@gmail.com>
2026-09-25 03:27:10 +0100

    phase2: add Docker build and test environment

---
+++ health.php
@@ -0,0 +1,5 @@
+<?php
+declare(strict_types=1);
+header('Content-Type: application/json');
+http_response_code(200);
+echo json_encode(['status' => 'ok', 'service' => 'skymart'], JSON_THROW_ON_ERROR);
